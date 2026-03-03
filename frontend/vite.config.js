6be45117ea9a6bf51c3c7df3ce6258be85b98762
Nicolas Gomez <nicolasgomez94@gmail.com>
2026-03-03 15:04:29 -0300

    Configurar frontend como static site

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: 5173,
-    strictPort: false,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-    strictPort: false,
   }
 })
