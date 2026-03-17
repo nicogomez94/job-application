4ab0e6fbad3c4e497be19f6dca81307ff23e4b2b
Nicolas Gomez <nicolasgomez94@gmail.com>
2026-03-16 22:52:29 -0300

    jwt

--- backend/src/config/jwt.js
+++ backend/src/config/jwt.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const generateToken = (payload, expiresIn = process.env.JWT_EXPIRES_IN) => {
+const generateToken = (payload, expiresIn = process.env.JWT_EXPIRES_IN || '7d') => {
   return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn });
 };
 
